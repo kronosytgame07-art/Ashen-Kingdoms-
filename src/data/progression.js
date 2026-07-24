19c4b4bca829a4d6d8494b12e4d9bd27ae73aa4a
kronosytgame07-art <kronosytgame07@gmail.com>
2026-07-24 08:38:11 +0200

    Add troop training quest

--- src/data/progression.js
+++ src/data/progression.js
@@ -7,6 +7,7 @@ export const TOWN_HALL_UNLOCKS = {
 export const QUESTS = [
   { id: 'build-gold', text: 'Construisez une Mine d’or corrompu', type: 'build', target: 'goldMine', reward: { gold: 120 } },
   { id: 'build-barracks', text: 'Élevez une Caserne maudite', type: 'build', target: 'barracks', reward: { wood: 150 } },
+  { id: 'train-skeleton', text: 'Entraînez votre premier Squelette-lancier', type: 'train', target: 'skeleton', value: 1, reward: { essence: 60 } },
   { id: 'upgrade-throne', text: 'Améliorez le Trône corrompu au niveau 2', type: 'level', target: 'townHall', value: 2, reward: { essence: 40 } },
   { id: 'build-vault', text: 'Construisez un Caveau d’âmes', type: 'build', target: 'soulVault', reward: { gold: 250, wood: 200 } }
 ];
